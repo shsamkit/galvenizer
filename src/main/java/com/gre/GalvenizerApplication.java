48b866188f4d1b0f93408ffd6d60b6b7e6ae8055
smackit <samshah1993@gmail.com>
2016-02-18 00:56:54 +0900

    Added application properties

--- src/main/java/com/gre/GalvenizerApplication.java
+++ src/main/java/com/gre/GalvenizerApplication.java
@@ -4,8 +4,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-import com.gre.services.WordServices;
-
 @SpringBootApplication
 @ComponentScan("com.gre")
 public class GalvenizerApplication {
